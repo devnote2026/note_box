77ea25146c922949b82324ff8d0291fc029ddeb0
Namitamy0503 <namitami162@gmail.com>
2026-03-15 02:08:37 +0900

    auth_service.dart実装

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,58 @@
+// ログインまわりの処理をまとめる
+
+//① Googleサインイン処理
+//② Googleサインアウト処理 
+//③ ログイン状態が変化したかを通知する処理
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'package:flutter/material.dart';
+
+class AuthService{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  
+
+  //Googleサインイン処理
+  Future <UserCredential?> signInWithGoogle() async {
+
+    try{
+      final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
+      
+      await _googleSignIn.initialize();
+
+      final GoogleSignInAccount googleUser = await _googleSignIn.authenticate();
+
+      final GoogleSignInAuthentication googleAuth =  googleUser.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken
+      );
+
+      return await _auth.signInWithCredential(credential);
+    }
+    
+    //Googleサインインに失敗したら処理を終了
+    catch(e){
+      debugPrint("Googleサインインに失敗: $e");
+      return null;
+    }
+  }
+
+
+  //サインアウト処理
+  Future<void> signOut() async {
+    try{
+      await GoogleSignIn.instance.signOut();
+      await _auth.signOut;
+    }
+    catch(e){
+      debugPrint("サインアウトエラー: $e");
+      return;
+    }
+  }
+
+  //ログイン状態の変化を通知する
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+}

cd3b06f51b237f8e280790a0f1f71e652b0ee52f
Namitamy0503 <namitami162@gmail.com>
2026-03-15 11:41:15 +0900

    ログイン画面完成(Appleログインはまだ)

--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../../services/auth_service.dart';
 
 
+// Googleログイン、Appleログインを行う。
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
@@ -11,49 +13,72 @@ class LoginScreen extends StatelessWidget {
     final _authService = AuthService(); 
 
     return Scaffold(
-      appBar: AppBar(title: Text("")),
+      appBar: AppBar(title: Text(""),backgroundColor: Colors.white,),
+      backgroundColor: Colors.white,
 
       body: Column(
         children: [
 
           Expanded(
             child: Center(
-              child: Text("")
+              child: Image.asset("assets/notebox_logo.png",height: 100,),
             )
           ),
 
           Padding(
             padding: EdgeInsets.all(24),
-            child: OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 56),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-            
-              onPressed: (){
-                _authService.signInWithGoogle();
-              },
-
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-
-                  Image.asset("assets/google_logo.png",height: 24,),
+            child: Column(
+              children: [
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 56),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onPressed: (){
+                    debugPrint('Appleログインは未実装');
+                  },
                   
-                  SizedBox(width: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/apple_logo.png",height: 24,),
 
-                  const Flexible(
-                    child: Text('Googleで続ける',overflow:  TextOverflow.ellipsis,)
+                      SizedBox(width: 12,),
+
+                      Text('Appleで続ける',style: TextStyle(color: Colors.black))
+                    ]
                   )
-                ],
-              )
+                ),
 
+                SizedBox(height: 12,),
 
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 56),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                   ),
+                  
+                  onPressed: (){
+                    _authService.signInWithGoogle();
+                  },
 
-              
-              )
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/google_logo.png",height: 24,),
+
+                      SizedBox(width: 12,),
+
+                      Text("Googleで続ける",style: TextStyle(color: Colors.black))
+                    ],
+                  )
+                )
+              ]
+            )
             )
           
         ]

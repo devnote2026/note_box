191d036b9b9baead9ae09fbeb1ed6ba023373e92
Namitamy0503 <namitami162@gmail.com>
2026-03-15 21:39:43 +0900

    画像アップロード関数実装ずみ

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
+import '../utils/image_utils.dart';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 class StorageService {
   final _storage = FirebaseStorage.instance;
@@ -9,7 +10,7 @@ class StorageService {
   Future<String> uploadProfileImage (String uid, File image) async {
     try{
 
-      final converted = _convertToJpg(image);
+      final converted = await ImageUtils.convertToJpg(image);
       final timestamp = DateTime.now().millisecondsSinceEpoch;
 
       final ref = _storage

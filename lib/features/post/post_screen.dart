1463e1986afcae7ad079036d2350e1a46308b7b1
Namitamy0503 <namitami162@gmail.com>
2026-03-19 00:54:35 +0900

    Deleteボタン追加

--- lib/features/post/post_screen.dart
+++ lib/features/post/post_screen.dart
@@ -17,6 +17,53 @@ class PostScreen extends StatefulWidget {
 class _PostScreenState extends State<PostScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+      body: SafeArea(                                  //安全なエリア
+        child: Column(
+          children: [
+
+            Expanded(
+              child: Stack(
+                children: [
+
+                  Positioned.fill(
+                    child: Image.file(
+                      widget.imageFile,
+                      fit:BoxFit.cover
+                    )
+                  ),
+
+                  //戻るボタン
+                  Positioned(
+                    bottom: MediaQuery.of(context).padding.bottom + 2,
+                    right: 16,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white, // 🔥 背景追加
+                        shape: BoxShape.circle,
+                      ),
+                      child: IconButton(
+                        icon:Icon(Icons.delete,color: Colors.black,size: 40,),
+                        onPressed: (){
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsetsGeometry.all(120),
+              child: Container(
+                color: Colors.black,
+              ),
+            )
+          ],
+        )
+      )      
+    );
   }
 }

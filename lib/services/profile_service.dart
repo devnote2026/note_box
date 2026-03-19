25dd427366332f085c05599b41184077990513f7
Namitamy0503 <namitami162@gmail.com>
2026-03-19 17:47:21 +0900

    学年・学科取得機能実装済み

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -4,12 +4,29 @@ import './storage_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-// プロフィール画像をストレージにアップロードし、URL、初回登録進捗状況更新
+
+//① ユーザーのプロフィールを取得する
+//② プロフィール画像をストレージにアップロードし、URL、初回登録進捗状況更新
 
 class ProfileService {
   final StorageService storageService;
 
   ProfileService(this.storageService);
+
+  
+  Future<Map<String,dynamic>> getProfile() async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
+    final doc = await FirebaseFirestore.instance  
+                                 .collection("users")
+                                 .doc(uid)
+                                 .get();
+    
+    return doc.data()!;
+  }
+
+
+
 
   Future<void> saveProfileImage (File image) async {
     final uid = FirebaseAuth.instance.currentUser!.uid;
